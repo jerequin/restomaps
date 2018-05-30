3fbfad3bae7b3e8651c7041bea15a42ecec493b8
jerLastra <jerlastra@gmail.com>
2018-05-31 01:26:11 +0800

    update changes in UI and code, slow loading because of foursqr no Drawing Yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,271 +1,210 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-let map, infoWindow, resultData, defaultLoc, marker;
-let restaurants = [];
-let allTypes = [];
-let allMarkers = [];
-let markerDetails;
-let uniqueTypes = [], circle, rectangle, markerOptions, circleOptions;
+var map, defaultLocation;
+var markers = [];
+
+var directionsService ;
+var directionsDisplay;
 
 function initMap() {
-  defaultLoc = new google.maps.LatLng(10.3157, 123.8854); // Cebu City
+  // Create the map.
+  defaultLocation = {lat: 10.3157, lng: 123.8854};
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: defaultLoc,
-    mapTypeId: 'roadmap'
+    center: defaultLocation,
+    zoom: 15
   });
 
-  infowindow = new google.maps.InfoWindow;
-
-  $('#types').on('change', function() { //changing types
-      clearMarkers();
-      setPlaces();
-  });
-  // getUserLocation() // used if you want to get your current location
-  getRestaurants();
-  draw()
-
-
-
-}
-
-function draw(){
-  var drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: null,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: [ 'circle']
-      },
-      // markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-      circleOptions: {
-        fillColor: '#044B94',
-        fillOpacity: 0.4,
-        strokeWeight: 5,
-        clickable: false,
-        editable: false,
-        zIndex: 1
-      }
-    });
-  drawingManager.setMap(map);
-
-  google.maps.event.addListener(drawingManager, 'circlecomplete', onCircleComplete);
-
-}
-
-
-function onCircleComplete(shape) {
-        if (shape == null || (!(shape instanceof google.maps.Circle))) return;
-
-        if (circle != null) {
-            circle.setMap(null);
-            circle = null;
-        }
-
-        circle = shape;
-
-        let newService = new google.maps.places.PlacesService(map);
-        let circlerestaurants;
-
-        newService.nearbySearch({
-            location: {lat : circle.getCenter().lat(), lng : circle.getCenter().lng() },
-            radius: circle.getRadius(),
-            type: ['restaurant'],
-        }, function callback(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                circlerestaurants = results;
-                console.log("circlerestaurants : ", circlerestaurants.length);
-
-                 circlerestaurants.forEach(function(resto){
-                  let retTypes = callMarker(resto);
-                  retTypes.forEach(function(types){
-                    allTypes.push(types);
-                  });
-                });
-                 getUniqueTypes(allTypes);
-            }
-            let restoNumbers = 0;
-            if (typeof circlerestaurants != "undefined") {
-              restoNumbers = circlerestaurants.length;
-            }
-
-
-            createCircleLabel(circle.getCenter().lat(), circle.getCenter().lng(), restoNumbers, circlerestaurants);
-        });
-        // console.log('radius', circle.getRadius());
-        // console.log('lat', circle.getCenter().lat());
-        // console.log('lng', circle.getCenter().lng());
-    }
-
-
-
-
-function getUserLocation(){
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
-
-
-// generate retaurants list
-function getList(newMarker) {
-    document.getElementById('list').innerHTML += '<span><strong>Restaurants:</strong> ' + newMarker.name +'</span><br>';
-}
-
-function getRestaurants() { // restaurants from google
-    service = new google.maps.places.PlacesService(map);
-    console.log("Default Location : ",defaultLoc);
-
-    let radiusVar = 5500;
-      service.nearbySearch({
-          location: defaultLoc,
-          radius: radiusVar,
-          type: ['restaurant'],
-      }, function callback(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-              restaurants = results;
-              // console.log("restaurants : ", restaurants);
-              restaurants.forEach(function(resto){
-                let retTypes = callMarker(resto);
-                retTypes.forEach(function(types){
-                  allTypes.push(types);
-                });
-              });
-
-              getUniqueTypes(allTypes);
-              setPlaces()
-
-          }
-      });
-}
-
-function getUniqueTypes(allTypes){
-  allTypes.forEach(function(x){
-    if($.inArray(x, uniqueTypes) === -1) 
-      uniqueTypes[x] = x;
-  });
-
-  uniqueTypes.unshift("All");
-
-  setTypes(uniqueTypes.sort()); 
-}
-
-function setTypes (uniqueTypes) { // setting of resto types in panel
-  let select = document.getElementById("types");
-  for(index in uniqueTypes) {
-      select.options[select.options.length] = new Option(uniqueTypes[index], index);
-  }
-
-}
-
-// go through each restaurants
-function setPlaces() {
-
-    let type = document.getElementById("types").value;
-    restaurants.forEach(function(resto){
-      if(resto.types.includes(type) == true)
-      {  
-        console.log("TRUE : ", resto.types);
-        clearMarkers();
-        callMarker(resto);
-      }
-    });
-}
-
-function createCircleLabel(lat, lng, restoNumbers, resto){
-
-
-   marker = new google.maps.Marker({
-      map: map,
-      position: {lat : lat, lng : lng},
-      label: restoNumbers.toString()
-  });
-
-   allMarkers[resto.id] = marker;
-   setMarkerInfo(marker, resto);
-}
-
-function callMarker(resto){
-
-  var icon = {
-      url: resto.icon,
-      scaledSize: new google.maps.Size(30, 30),
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
   };
 
+  // Perform a nearby search.
+  service.nearbySearch(
+  {
+    location: defaultLocation, 
+    radius: 1000, 
+    type: ['restaurant']
+  },
+  function(results, status, pagination) {
+    if (status !== 'OK') return;
 
-  marker = new google.maps.Marker({
-      icon: icon,
-      map: map,
-      position: resto.geometry.location,
-      title: resto.name
+    createMarkers(results);
+    moreButton.disabled = !pagination.hasNextPage;
+    getNextPage = pagination.hasNextPage && function() {
+      pagination.nextPage();
+    };
   });
 
-  marker.id = resto.id;
-  marker.name = resto.name;
+  $('#types').on('change', function() { //changing types
+      deleteMarkers();
+      deleteLists();
+      filterTypes();
+  });
 
-  allMarkers[resto.id] = marker;
-
-  getList(marker);
-
-  let venuesFS = callFourSquare(resto.geometry.location.toUrlValue(7), resto.name);
-    
-
-  setMarkerInfo(marker,resto,venuesFS);
-
-  return resto.types;
+  $('.get-direction').on('click', function() { //changing types
+      deleteDestiDisplay();
+  });
 
 }
 
-function setMarkerInfo(newMarker,resto,venuesFS = {}){
+function deleteDestiDisplay(){
+  directionDisplay.setPanel(null);
+}
 
+function filterTypes() {
+  // Create the map.
+  let types = document.getElementById("types").value;
+
+
+  defaultLocation = {lat: 10.3157, lng: 123.8854};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: defaultLocation,
+    zoom: 15
+  });
+
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
+  };
+
+  // Perform a nearby search.
+  service.nearbySearch(
+  {
+    location: defaultLocation, 
+    radius: 1000, 
+    type: [types]
+  },
+  function(results, status, pagination) {
+    if (status !== 'OK') return;
+
+    createMarkers(results);
+    moreButton.disabled = !pagination.hasNextPage;
+    getNextPage = pagination.hasNextPage && function() {
+      pagination.nextPage();
+    };
+  });
+
+
+}
+
+function deleteLists(places) {
+  document.getElementById('places').innerHTML = '';
+
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+
+
+
+  for (var i = 0, place; place = places[i]; i++) {
+    // console.log("place : ", place);
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+
+    markers.push(marker);
+
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    var placeVicinity = (place.vicinity) ?  '<a href="#" id="get-direction-' + place.id + '" class="get-direction">'+ place.name+'GO HERE!</a><br><br>' : '' ;
+    a.textContent = place.name;
+    a.setAttribute("id", 'get-direction-' + place.id )
+    a.setAttribute("class", 'get-direction' + place.id )
+    li.appendChild(a);
+    placesList.appendChild(li);
+
+    bounds.extend(place.geometry.location);
+
+    let venues = callFourSquare(place.geometry.location.toUrlValue(5), place.name);
+
+    setMarkerInfo(marker,place, venues); // set marker info 
+
+
+  }
+  map.fitBounds(bounds);
+
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+
+function setMarkerInfo(newMarker, place, venues){
+
+  var infowindow = new google.maps.InfoWindow;
   var opening = '';
-    if (resto.opening_hours) {
-        if (resto.opening_hours.open_now) {
+    if (place.opening_hours) {
+        if (place.opening_hours.open_now) {
             opening = '<strong><span style="color: green;">Open now</span></strong><br>';
         } else {
             opening = '<strong><span style="color: red;">Closed</span></strong><br>';
         }
     }
 
-  var restoName = (resto.name) ? '<strong>' + resto.name + ' </strong><br><br>' : '' ;
-    var restoVicinity = (resto.vicinity) ? resto.vicinity + '<br> <a href="#" id="get-direction-' + resto.id + '" class="get-direction">GO HERE and see directions!</a><br><br>' : '' ;
-    var specialty = (resto.specialty) ? 'Specialty : ' + resto.specialty + '<br>' : '' ;
-    var restoTypes = (resto.types) ? 'Restaurant type : ' + resto.types + '<br>' : '';
-    var rating = (resto.specialty) ? 'Rating : ' + resto.specialty + '<br>' : '' ;
-    var checkinCount = (venuesFS.checkinCount) ? 'Checkin Count : ' + venuesFS.checkinCount + '<br>': "";
-    var customerCount = (venuesFS.customerCount) ? 'Customer Count : ' + venuesFS.customerCount + '<br>': "";
+  var placeName = (place.name) ? '<strong>' + place.name + ' </strong><br><br>' : '' ;
+    var placeVicinity = (place.vicinity) ? place.vicinity + '<br> <a href="#" id="get-direction-' + place.id + '" class="get-direction">GO HERE and see directions!</a><br><br>' : '' ;
+    var specialty = (place.specialty) ? 'Specialty : ' + place.specialty + '<br>' : '' ;
+    var placeTypes = (place.types) ? 'Restaurant type : ' + place.types + '<br>' : '';
+    var rating = (place.specialty) ? 'Rating : ' + place.specialty + '<br>' : '' ;
 
+    var from = "";
+    if (venues != null){
+      var from = "<strong>From FOURSQUARE :</strong><br>";
+      var checkinCount = (venues.checkinCount) ? 'Checkin Count : ' + venues.checkinCount + '<br>': "";
+      var customerCount = (venues.customerCount) ? 'Customer Count : ' + venues.customerCount + '<br>': "";
+
+    }
+    
+
+    // console.log(checkinCount + "," + customerCount);
     // add on click event on markers to show restaurant details
     google.maps.event.addListener(newMarker, 'click', function() {
         infowindow.setContent('<div>' + 
-            restoName + '' +
-            restoVicinity +
+            placeName + '' +
+            placeVicinity +
             opening +
-            restoTypes +
+            placeTypes +
             specialty +
-            rating +
+            rating + "<br>" + 
+            from +
             checkinCount +
             customerCount
         );
@@ -273,8 +212,47 @@ function setMarkerInfo(newMarker,resto,venuesFS = {}){
     });
 
     // on click get direction
-    $(document).off('click', '#get-direction-' + resto.id).on('click', '#get-direction-' + resto.id, function() {
-        calculateAndDisplayRoute(resto.geometry.location, resto.name);
+    $(document).off('click', '#get-direction-' + place.id).on('click', '#get-direction-' + place.id, function() {
+
+        calculateAndDisplayRoute(place.geometry.location, place.name);
+        deleteDestiDisplay();
+
+    });
+}
+
+// get and set destination
+function calculateAndDisplayRoute(destination, name) {
+    // directions request on service
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
+
+    directionsDisplay.setMap(map);
+    directionsService.route({
+      origin: defaultLocation,
+      destination: destination,
+      waypoints: [],
+      optimizeWaypoints: true,
+      travelMode: 'DRIVING'
+    }, 
+    function(response, status)
+    {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+        var route = response.routes[0];
+        var summaryPanel = document.getElementById('directions-panel');
+        summaryPanel.innerHTML = '';
+        // For each route, display summary information.
+        for (var i = 0; i < route.legs.length; i++) {
+          var routeSegment = i + 1;
+          summaryPanel.innerHTML += '<b>Route : ' + routeSegment +
+              '</b><br>';
+          summaryPanel.innerHTML += "START " + route.legs[i].start_address + ' to <br>';
+          summaryPanel.innerHTML += "END " + route.legs[i].end_address + '<br>';
+          summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+        }
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
     });
 }
 
@@ -295,7 +273,6 @@ function callFourSquare(coordinates, restoName) { // get by coordinates and name
 
     let returnResponse = {};
 
-    // console.log(response.response.venues);
     if(response){
       returnResponse.customerCount = (typeof response.response.venues[0].stats.usersCount != "undefined") ? response.response.venues[0].stats.usersCount : "";
       returnResponse.checkinCount = (typeof response.response.venues[0].stats.checkinsCount != "undefined") ? response.response.venues[0].stats.checkinsCount : "";
@@ -308,50 +285,3 @@ function callFourSquare(coordinates, restoName) { // get by coordinates and name
   }
 }
 
-// clear markers
-function clearMarkers() {
-
-    $.each(allMarkers, function(i, marker) {
-        marker.setMap(null);
-    });
-
-    allMarkers = {};
-}
-
-// get and set destination
-function calculateAndDisplayRoute(destination, name) {
-    // directions request on service
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsDisplay.set('directions', null);
-    directionsDisplay.setMap(null);
-    directionsDisplay.setDirections(null);
-
-    directionsDisplay.setMap(map);
-  directionsService.route({
-    origin: defaultLoc,
-    destination: destination,
-    waypoints: [],
-    optimizeWaypoints: true,
-    travelMode: 'DRIVING'
-  }, function(response, status) {
-    if (status === 'OK') {
-      directionsDisplay.setDirections(response);
-      var route = response.routes[0];
-      console.log(route);
-      var summaryPanel = document.getElementById('directions-panel');
-      summaryPanel.innerHTML = '';
-      // For each route, display summary information.
-      for (var i = 0; i < route.legs.length; i++) {
-        var routeSegment = i + 1;
-        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-            '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-      }
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
